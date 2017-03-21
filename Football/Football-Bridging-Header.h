8b218c7583850a0e79fe29f8227735fd0bc1bf2d
Udoy.Hossen <miltan17.ruet@gmail.com>
2017-03-21 10:05:00 +0600

    # Added slider menu item

---
+++ Football/Football-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Football-Bridging-Header.h
+//  Football
+//
+//  Created by MbProRetina on 3/21/17.
+//  Copyright © 2017 MbProRetina. All rights reserved.
+//
+
+#ifndef Football_Bridging_Header_h
+#define Football_Bridging_Header_h
+
+#import "SWRevealViewController.h"
+
+#endif /* Football_Bridging_Header_h */
--- Football/ViewController.swift
+++ Football/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menu: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        menu.target = self.revealViewController()
+        menu.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
     }
 
     override func didReceiveMemoryWarning() {
